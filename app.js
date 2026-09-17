eecb6e4343aa122773cbe4e6fad4bcb351064441
myane234 <flytothemoonkawaii@gmail.com>
2026-09-17 12:46:21 +0700

    add version

--- app.js
+++ app.js
@@ -81,6 +81,7 @@ async function mainMenu() {
 `),
   );
   // console.log('[1] Input API');
+  console.log('Versi: 1.8')
   console.log('[2] Start Automation');
   console.log('[3] Generate Prompt dari folder Download + Generate gambar');
   console.log('[4] Test Koneksi');
